195d8e228df39d85412db96cb9c550d97c001e8f
zifan feng <zifanfeng@host-229-216.ilcupre.champaign.il.us.clients.pavlovmedia.net>
2017-02-04 09:03:34 -0600

    E1: Commit arithmeam

---
+++ scripts/arith_mean.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# Computes the arithmetic mean
+#1 Path to file containing numeric values one per line
+arith_mean() {
+
+  sum=0                     # Hold summation
+  fname=$1                  # File to take estimate of
+  count=$(wc -l < "$fname") # count number of lines
+
+  while read line
+  do
+    sum=$((sum+line))      # Sum
+  done < "$fname"
+
+  if [ "$count" != 0 ]; then
+    mu=$((sum/count))
+    echo "Sum = $sum, Count = $count, Mean = $mu"
+  else
+    echo "Please supply a file with more than 1 line of numerical input."
+  fi
+  
+}
+
+arith_mean $1             # Function call
